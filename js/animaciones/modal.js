2675fb9b77e811498796ca6deaa060efb9ee42ca
alexanderoficial <138260223+alexanderoficial@users.noreply.github.com>
2024-07-01 20:55:37 -0500

    Update modal.js
    modifcacion del js y tamaño de pantalla

--- js/animaciones/modal.js
+++ js/animaciones/modal.js
@@ -2,10 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
     var modal = document.getElementById("termsModal");
     var acceptBtn = document.getElementById("acceptBtn");
 
-    modal.style.display = "flex";
+    if (window.innerWidth > 1023) {
+        modal.style.display = "flex";
+    }
 
     acceptBtn.onclick = function() {
         modal.style.display = "none";
     }
 });
-
