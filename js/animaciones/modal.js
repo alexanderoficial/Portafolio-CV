8eb3fc01d439968036272d585a0594144bd99d99
alexanderoficial <138260223+alexanderoficial@users.noreply.github.com>
2024-07-01 20:36:53 -0500

    Update modal.js
    js jsj sjjssjjsj

--- js/animaciones/modal.js
+++ js/animaciones/modal.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var modal = document.getElementById("termsModal");
+    var acceptBtn = document.getElementById("acceptBtn");
+
+    modal.style.display = "flex";
+
+    acceptBtn.onclick = function() {
+        modal.style.display = "none";
+    }
+});
 
